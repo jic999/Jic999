324344205f2eb32ec1aeb9664f1f2aa25f34c433
jic999 <jic999@qq.com>
2023-10-06 19:35:07 +0800

    feat: add footer

--- .vitepress/theme/components/JFooter.vue
+++ .vitepress/theme/components/JFooter.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <footer>
-    <div />
+  <footer py-12 text="sm center $text-c-1">
+    <p>
+      <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>
+      2023-PRESENT © Jic999
+    </p>
   </footer>
 </template>
